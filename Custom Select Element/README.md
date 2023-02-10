90fd9ea4948e8c36a31f3e13bdbc780653ebd1dc
AZP <fendt936ap@gmail.com>
2023-02-10 17:13:17 +0100

    Update #01
    Add the arrow to say 'this is a select' and CodePen link added

--- Custom Select Element/README.md
+++ Custom Select Element/README.md
@@ -1,3 +1,9 @@
 # Custom Select element
 
-A custom select element
+A custom select element  
+Made by SoftGuy [GitHub](https://github.com/AntonioSG41) / [CodePen](https://codepen.io/AntonioSG41)
+
+---
+
+#### Source code aviable at
+[GitHub](https://github.com/AntonioSG41/HTMLTests/tree/main/Custom%20Select%20Element) / [CodePen](https://codepen.io/AntonioSG41/pen/dyjEVJd)
--- Custom Select Element/script.js
+++ Custom Select Element/script.js
@@ -1,18 +1,20 @@
 const select = {};
 
 select.init = (id) => {
-    document.querySelector('#' + id + ' > .select-btn').addEventListener('click', () => { select.showOptions(id); });
-    document.querySelectorAll('#' + id + ' > .select-options > .select-option').forEach(elem => {
+    let selectBtn = document.querySelector(`#${id} > .select-btn`);
+    selectBtn.addEventListener('click', () => { select.showOptions(id); });
+    document.querySelectorAll(`#${id} > .select-options > .select-option`).forEach(elem => {
         elem.addEventListener('click', () => { select.setValue(id, elem.getAttribute('data-val')) });
     });
+    document.querySelector(`#${id} > .select-options`).style.minWidth = selectBtn.style.width;
 };
 
 select.showOptions = (id) => {
-    document.querySelector('#' + id + ' > .select-options').style.display = 'block';
+    document.querySelector(`#${id} > .select-options`).style.display = 'block';
 };
 
 select.getValue = (id) => {
-    let val = document.querySelector('#' + id + ' > .select-val').innerText;
+    let val = document.querySelector(`#${id} > .select-val`).innerText;
     if (val == '')
         return undefined;
     return val;
@@ -20,9 +22,9 @@ select.getValue = (id) => {
 
 select.setValue = (id, value) => {
     let optionElement = document.querySelector(`#${id} > .select-options > .select-option[data-val="${value}"]`);
-    document.querySelector('#' + id + ' > .select-val').innerText = value;
-    document.querySelector('#' + id + ' > .select-btn').innerText = optionElement.innerText;
-    document.querySelector('#' + id + ' > .select-options').style.display = 'none';
+    document.querySelector(`#${id} > .select-val`).innerText = value;
+    document.querySelector(`#${id} > .select-btn > span`).innerText = optionElement.innerText;
+    document.querySelector(`#${id} > .select-options`).style.display = 'none';
     document.querySelectorAll(`#${id} > .select-options > .select-option.active`).forEach(elem => elem.classList.remove('active'));
     optionElement.classList.add('active');
 };
